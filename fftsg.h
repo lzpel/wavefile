a32be3418b127d73bb532e4b0425ca70f23cec07
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2019-06-27 16:00:31 +0900

    uupdate

--- fftsg.h
+++ fftsg.h
@@ -30,32 +30,49 @@ void dfst(int, double *, double *, int *, double *);
 
 class FFT {
 public:
-	double *buf, *w;
+	double *w;
 	signed size;
 	signed *ip;
+	double *buf0,*buf1,*buf2;
 
 	FFT(int s) {
 		size = s;
 		ip = new int[2 + (int) sqrt(s / 2)];
-		buf = new double[s];
 		w = new double[s / 2];
 		ip[0] = 0;
+		alloc(buf0);
+		alloc(buf1);
+		alloc(buf2);
 	}
 
 	~FFT() {
 		delete[] ip;
-		delete[] buf;
 		delete[] w;
+		free(buf0);
+		free(buf1);
+		free(buf2);
 	}
 
 	void rdft(double *a) {
+		//実数離散フーリエ変換
 		::rdft(size, 1, a, ip, w);
+		//振幅に変換
+		for (int j = 0; j < size; j++) a[j] *= 2.0 / size;
 	}
 
 	void irdft(double *a) {
 		::rdft(size, -1, a, ip, w);
-		//sum_0^s f_t^2 =(sum_0^s/2 F_a^2)/(s/2)
-		for (int j = 0; j < size; j++) a[j] *= 2.0 / size;
+	}
+
+	double power(double *d, bool amp) {
+		double s=0,a=0;
+		if(amp){
+			for (int i = 1; i < size; ++i)s+=d[i]*d[i]*size/2.0;
+		}else{
+			for (int i = 0; i < size; ++i)a+=d[i]/size;
+			for (int i = 0; i < size; ++i)s+=(d[i]-a)*(d[i]-a);
+		}
+		return s;
 	}
 
 	void alloc(double *&p) {
@@ -68,21 +85,6 @@ public:
 
 	static void free(double *p) {
 		delete[] p;
-	}
-
-	double power(double *d, bool spectrum) {
-		return power(d, spectrum, size);
-	}
-
-	static double power(double *d, bool spectrum, int len) {
-		double s=0,a=0;
-		if(spectrum){
-			for (int i = 1; i < len; ++i)s+=d[i]*d[i]/len*2;
-		}else{
-			for (int i = 0; i < len; ++i)a+=d[i]/len;
-			for (int i = 0; i < len; ++i)s+=(d[i]-a)*(d[i]-a);
-		}
-		return s;
 	}
 
 	void copy(double *into, const double *from) {
@@ -110,12 +112,12 @@ public:
 		for (int i = 0; i < size; ++i)p[i] *= 1 - cos(2 * M_PI * i / size);
 	}
 
-	void print(const char *fn, const double *p, bool row) {
-		print(fn, p, row, size);
+	void print(const char *fn, const double *p) {
+		print(fn, p, size);
 	}
 
-	static void print(const char *fn, const double *p, bool row, int len) {
-		const char *fmt = row ? "%f\n" : "%f ";
+	static void print(const char *fn, const double *p, int len) {
+		const char *fmt = "%f\n";
 		if (fn) {
 			FILE *f = fopen(fn, "w");
 			for (int i = 0; i < len; i++)fprintf(f, fmt, p[i]);
@@ -125,7 +127,8 @@ public:
 		}
 	}
 
-	void spectrum_cos(double *w) {
+	void spectrum_amp(double *w) {
+		//位相なし振幅スペクトルに変換
 		w[0] = w[1] = 0;
 		for (int i = 1; i < size / 2; ++i) w[i] = w[i * 2] * w[i * 2] + w[i * 2 + 1] * w[i * 2 + 1];
 		for (int i = 0; i < size / 2; ++i) {
@@ -135,29 +138,24 @@ public:
 	}
 
 	void spectrum_log(double *w) {
-		w[0] = w[1] = 0;
-		for (int i = 1; i < size / 2; ++i) w[i] = w[i * 2] * w[i * 2] + w[i * 2 + 1] * w[i * 2 + 1];
-		for (int i = 0; i < size / 2; ++i) {
-			w[i + size / 2] = 0;
-			w[i] = w[i] ? log(w[i]) / 2 : 0;
-		}
+		//位相なし対数振幅スペクトルに変換
+		spectrum_amp(w);
+		for (int i = 0; i < size / 2; ++i) w[i] = w[i]?log(w[i]):0;
 	}
 
-	void spectrum_aperiodicity(double *w, int n) {
-		w[0] = w[size / 2] = 0;
-		for (int i = 1; i < size / 2 / n; i++) {
-			const int npos = i * n - (n - 1) / 2;
-			double min = DBL_MAX, max = DBL_MIN;
-			for (int j = npos; j < npos + n; j++) {
-				min = MIN(min, w[j]);
-				max = MAX(max, w[j]);
-			}
-			w[i] = max;
-			w[i + size / 2] = min;
+	void spectrum_log_periodseparate(double*periodicity,double*aperiodicity,double *spectrum, int n) {
+		//雑にf0振幅の補正
+		spectrum[0]=spectrum[1];
+		double*average=periodicity,*variance=aperiodicity,*amplitude=aperiodicity;
+		for(int i=0;i<size;i++)average[i]=variance[i]=0;
+		for(int i=n;i<size/2;i++){
+			for(int j=i-(n-1)/2;j<n;j++)average[i]+=spectrum[j]/n;
+			for(int j=i-(n-1)/2;j<n;j++)variance[i]+=(spectrum[j]-average[i])*(spectrum[j]-average[i])/n;
+			amplitude[i]=sqrt(variance[i]*2);
+			periodicity[i]=average[i]+amplitude[i];
+			aperiodicity[i]=average[i]-amplitude[i];
 		}
-		for (int i = size / 2 / n; i < size / 2; i++) {
-			w[i] = w[i + size / 2] = 0;
-		}
+		spectrum[0]=0;
 	}
 
 	void zero(double *t) {
@@ -227,10 +225,6 @@ public:
 			}
 		}
 		return 0;
-	}
-
-	static void zerocrosslenarray(int lenmin, int lenmax, double *pd, double *ps, int pn) {
-		for (int i = 0; i < pn - lenmax * 1.5; i++)pd[i] = zerocrosslen(lenmin, lenmax, ps + i);
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -4,32 +4,34 @@
 
 void test1(int f0){
 	static const int n=3;
-	double *p, *p2, *t;
+	double *p, *p2;
 	signed l, r;
 	waveload("../test.wav", &l, &r, &p);
 	FFT fft(1024);
-	fft.print("out10.csv", p, 1, l);
+	fft.print("out10.csv", p, l);
 	fft.alloc(p2,l);
-	fft.alloc(t);
-	fft.fir(t, r / 400, r / 200);
-	fft.conv(p2, p, l, t, true);
+	fft.fir(fft.buf0, r / 400, r / 200);
+	fft.conv(p2, p, l, fft.buf0, true);
 	wavesave("out10.wav", l, r, p2);
-	for(int i=0;i<(l/fft.size);i++){
-		signed pos=i*fft.size,t0;
-		if(f0){
-			t0=r/f0;
-		}else{
-			t0=fft.zerocrosslen(r/400,r/200,p2+pos);
+	for(int i=0;i<l;i++){
+		if(i%fft.size==0) {
+			signed t0;
+			if (f0) {
+				t0 = r / f0;
+			} else {
+				t0 = fft.zerocrosslen(r / 400, r / 200, p2 + i);
+				t0 = MAX(r / 400, t0);
+			}
+			fft.interpolate(fft.buf0, fft.size, p + i, t0 * n);
+			//fft.window(fft.buf0);窓を使うと非周期成分が見えない
+			fft.rdft(fft.buf0);
+			fft.spectrum_log(fft.buf0);
+			fft.spectrum_log_periodseparate(fft.buf1, fft.buf2, fft.buf0, n);
+			fft.print("out11.csv",fft.buf0);
+			fft.print("out12.csv",fft.buf1);
+			fft.print("out13.csv",fft.buf2);
 		}
-		fft.interpolate(t,fft.size,p+pos,t0*n);
-		//fft.window(t);窓を使うと非周期成分が見えない
-		fft.rdft(t);
-		fft.spectrum_log(t);
-		fft.spectrum_aperiodicity(t,n);
-		fft.copy(p+pos,t);
 	}
-	fft.print("out11.csv", p, 1, l);
-	fft.free(t);
 	fft.free(p2);
 }
 void test2(){
@@ -40,12 +42,15 @@ void test2(){
 	printf("%f\n",fft.power(t,0));
 	fft.rdft(t);
 	printf("%f\n",fft.power(t,1));
-	fft.spectrum_cos(t);
+	fft.spectrum_amp(t);
 	printf("%f\n",fft.power(t,1));
 	fft.free(t);
 }
+void test3(){
+
+}
 int main() {
-	//test1(306);
+	test1(0);
 	test2();
 	return 0;
 }
